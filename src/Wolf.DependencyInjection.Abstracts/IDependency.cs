8e3fec540508d8dc08ee8bff2c131f3c3aca2ec1
zhenlei520 <wangzhenlei520@gmail.com>
2021-07-05 21:44:21 +0800

    新增发布脚本

--- src/Wolf.DependencyInjection.Abstracts/IDependency.cs
+++ src/Wolf.DependencyInjection.Abstracts/IDependency.cs
@@ -4,78 +4,306 @@
 
 namespace Wolf.DependencyInjection.Abstracts
 {
+    /// <summary>
+    ///
+    /// </summary>
     public interface IDependency
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IDependency<T>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
     public interface IDependency<T1, T2>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
     public interface IDependency<T1, T2, T3>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
     public interface IDependency<T1, T2, T3, T4>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
+    /// <typeparam name="T16"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
+    /// <typeparam name="T16"></typeparam>
+    /// <typeparam name="T17"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17>
     {
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
+    /// <typeparam name="T16"></typeparam>
+    /// <typeparam name="T17"></typeparam>
+    /// <typeparam name="T18"></typeparam>
     public interface IDependency<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18>
     {
     }
